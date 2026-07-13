ffa6092d422e635c3435f4410beba5035fb7fe89
english-uor <english.basic.edu@uor.edu.krd>
2026-07-13 13:07:46 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = 'enghub-portals-v1.2';
+const ASSETS_TO_CACHE = [
+    './index.html',
+    './teachers.html',
+    './student_portal.html',
+    './holidays.html',
+    './manifest.json',
+    './admin-config.json',
+    './image_0cf63c.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('EngHub Cache Opened Successfully');
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting obsolete cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
